81af1b9162b15ba47e829a7e46b8327fc9319e27
prianon.jeremy@gmail.com <jprianon@mareva.com>
2024-10-20 17:30:19 +0400

    Add catalogue 95

--- scripts/check_changes.sh
+++ scripts/check_changes.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-REPO_SPECIFIC_PATH=$1
+# Définir le chemin spécifique au répertoire
+REPO_SPECIFIC_PATH="catalogue/"
 
-# Vérifie les fichiers modifiés dans la branche
+# Vérifier si le répertoire existe
+if [ ! -d "$REPO_SPECIFIC_PATH" ]; then
+    echo "$REPO_SPECIFIC_PATH n'existe pas."
+    exit 1
+fi
+
+# Vérifier les changements
 changes=$(git diff --name-only HEAD~1 HEAD)
-echo $changes
 
-# Si les changements incluent des fichiers du répertoire spécifique, déclencher le webhook
+echo "Changements détectés : $changes"
+
+# Si aucune modification n'est trouvée, quitter le script
+if [[ -z "$changes" ]]; then
+    echo "Aucune modification détectée. Quitter le build."
+    exit 0
+fi
+
+# Vérifier si les changements sont dans le répertoire spécifique
 if [[ "$changes" == *"$REPO_SPECIFIC_PATH"* ]]; then
-  curl -X POST -H "Content-Type: application/json" -d '{"text":"Modifications détectées dans '"$REPO_SPECIFIC_PATH"'"}' https://codebuild.eu-west-1.amazonaws.com/webhooks?t=eyJlbmNyeXB0ZWREYXRhIjoiR0hXMmZTZWNhTDExalVFdmllRU8yN2RWNnBEN0NEWDFxOVlvM1NFUGF5a2YrQysvMHp2RytpalZScHNEWG12OHVZWnBZV09ZRk5MeWQwQkVKM3FxNFRvPSIsIml2UGFyYW1ldGVyU3BlYyI6InpSZkhTTURCWGorM0pHQUwiLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&v=1
+    echo "Modifications détectées dans '$REPO_SPECIFIC_PATH'"
+    # Appeler le webhook ou d'autres étapes si nécessaire
+    curl -X POST -H "Content-Type: application/json" -d '{"text":"Modifications détectées dans '"$REPO_SPECIFIC_PATH"'"}' hhttps://codebuild.eu-west-1.amazonaws.com/webhooks?t=eyJlbmNyeXB0ZWREYXRhIjoiR0hXMmZTZWNhTDExalVFdmllRU8yN2RWNnBEN0NEWDFxOVlvM1NFUGF5a2YrQysvMHp2RytpalZScHNEWG12OHVZWnBZV09ZRk5MeWQwQkVKM3FxNFRvPSIsIml2UGFyYW1ldGVyU3BlYyI6InpSZkhTTURCWGorM0pHQUwiLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&v=1
 else
-  echo "Aucune modification dans le répertoire spécifique $REPO_SPECIFIC_PATH"
+    echo "Aucune modification dans le répertoire spécifique '$REPO_SPECIFIC_PATH'"
 fi
